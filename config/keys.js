0ba0fabd33009161d9aa7b33b92c8c3de4be3b46
iritashuri <iritashuri5109@gmail.com>
2021-03-05 12:02:25 +0200

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://IritAshuri:irit1234@cluster0.fwhrx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+// module.exports = {
+//     mongoURI: dbPassword
+// };
