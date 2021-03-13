4cabe0e7175ab134060c9a19b9bf93f5024e6acf
iritashuri <iritashuri5109@gmail.com>
2021-03-13 12:11:01 +0200

    add tabs

---
+++ public/js/tabs.js
@@ -0,0 +1,21 @@
+function openLevels(evt, level) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("nav-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        console.log(tablinks[i].className);
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(level).style.display = "block";
+    evt.currentTarget.className += " active";
+}
