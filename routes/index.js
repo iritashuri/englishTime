e5d5d1783ac3a5fbbadbff40b15f646d186593ef
iritashuri <iritashuri5109@gmail.com>
2021-03-21 14:52:17 +0200

    tart snake game

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
+router.get('/snake', (req, res, next) =>
+  res.render('snake', { user: 'USER', category: 'CATEGORY' })
+);
+
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
