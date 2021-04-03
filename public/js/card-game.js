da4795d516b6d8b69a075ccdaa8905cf0800a40c
Harel Braha <rlbraha10@gmail.com>
2021-04-03 14:20:28 +0300

    game2: add points and lifes
    add points, life and right left bound

--- public/js/card-game.js
+++ public/js/card-game.js
@@ -6,33 +6,39 @@ if (document.URL.includes('card-game')) {
   var enemies=[];
 
   function start_game(){
+    score=0;
+    life=4;
      hero = {
         left: 575,
         top: 700
     };
 
      missiles = [];
+     restart_enemies();
+     
+  }
 
-     enemies = [
-        { left: 200, top: 100 },
-        { left: 300, top: 100 },
-        { left: 400, top: 100 },
-        { left: 500, top: 100 },
-        { left: 600, top: 100 },
-        { left: 700, top: 100 },
-        { left: 800, top: 100 },
-        { left: 900, top: 100 },
-    ];
+  function restart_enemies(){
+    enemies = [
+      { left: 200, top: 100 },
+     // { left: 300, top: 100 },
+     // { left: 400, top: 100 },
+     // { left: 500, top: 100 },
+      { left: 600, top: 100 },
+      //{ left: 700, top: 100 },
+      //{ left: 800, top: 100 },
+      { left: 900, top: 100 },
+  ];
   }
 
     document.onkeydown = function(e) {
-        if (e.keyCode === 37) {
+        if (e.keyCode === 37 && hero.left>0) {
             // Left
-            hero.left = hero.left - 10;
+            hero.left = hero.left - 20;
         }
-        if (e.keyCode === 39) {
+        if (e.keyCode === 39 && hero.left<1150) {
             // Right
-            hero.left = hero.left + 10;
+            hero.left = hero.left + 20;
         }
         if (e.keyCode === 32) {
             // Spacebar (fire)
@@ -67,8 +73,9 @@ if (document.URL.includes('card-game')) {
   function drawEnemies() {
       document.getElementById('enemies').innerHTML = ""
       for(var i = 0 ; i < enemies.length ; i++ ) {
-          document.getElementById('enemies').innerHTML += `<div class='enemy' style='left:${enemies[i].left}px; top:${enemies[i].top}px'></div>`;
-      }
+          document.getElementById('enemies').innerHTML +=
+           `<div class='enemy' style='left:${enemies[i].left}px; top:${enemies[i].top}px'>`+user.name+`</div>`;
+        }
   }
 
   function moveEnemies() {
@@ -88,36 +95,56 @@ if (document.URL.includes('card-game')) {
               ) {
                   enemies.splice(enemy, 1);
                   missiles.splice(missile, 1);
+                  console.log(enemies)
+                  console.log(missiles)
+                  score++;
+                  console.log(score);
+
               }
           }
       }
   }
 
-  function check_game(ti){
-      if(enemies[0].top > 650){
+  function check_game(timeout){
+      if(enemies[0].top > 650 && life>0){
         console.log('inside2')
-        alert("Game Over");
-        clearTimeout(ti);
-        start_game();
-        gameLoop();
+        life--;
+        restart_enemies();
+        console.log("life: "+life);
+        if(life==0){
+          alert("Game Over");
+          clearTimeout(timeout);
+          start_game();
+          gameLoop();
+        }
           
 
       }
 
   }
 
+  function all_enemie_dead(){
+    if(enemies.length==0){
+      restart_enemies()
+    }
+  }
+
   function gameLoop() {
-      var ti= setTimeout(gameLoop, 5)
+      var timeout= setTimeout(gameLoop, 50)
+      all_enemie_dead();
       moveMissiles();
       drawMissiles();
       moveEnemies();
-      check_game(ti);
+      check_game(timeout);
       drawEnemies();
       collisionDetection();
   }
 
   var gameover=0;
+  var score;
+  var life;
   start_game()
+  alert("start game");
   gameLoop()
 
 
